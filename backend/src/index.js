89301897dc364c56013457bb06c34905cc9b0c3b
Duane Chan <duanechan19@gmail.com>
2026-04-08 00:19:51 +0800

    chore: add REST convention

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,12 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(logMiddleware);
 
-app.use("/auth", authRouter);
-app.use("/products", authMiddleware, productRouter);
-
-app.get("/", authMiddleware, (req, res) => {
-  res.send("Hello, world!");
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/products", authMiddleware, productRouter);
 
 app.listen(config.port, () =>
   logger.info(`Server running on port ${config.port}`),
