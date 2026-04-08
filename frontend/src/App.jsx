cb063eb5a08cf25cf0f7cb8584672f0cc6dfee26
Duane Chan <duanechan19@gmail.com>
2026-04-08 13:31:42 +0800

    feat: implement login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  RouterProvider,
+} from "@tanstack/react-router";
 import Login from "./pages/Login.jsx";
+
+const queryClient = new QueryClient();
+
+const rootRoute = createRootRoute();
+const loginRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/login",
+  component: Login,
+});
+
+const routeTree = rootRoute.addChildren([loginRoute]);
+const router = createRouter({ routeTree });
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,26 +1,81 @@
+import { EyeIcon, EyeSlashIcon } from "@phosphor-icons/react";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "@tanstack/react-router";
+import { useState } from "react";
+
 export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+  const {
+    mutate: login,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: (credentials) =>
+      fetch("/api/v1/auth/login", {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      }).then((res) => {
+        if (!res.ok) throw new Error("Login failed");
+        return res.json();
+      }),
+    onSuccess: (data) => {
+      navigate({ to: "/price-list" });
+    },
+  });
+
+  function handleLogin(e) {
+    e.preventDefault();
+    if (email.trim() === "" || password.trim() === "") {
+      return;
+    }
+
+    login({ email, password });
+  }
+
   return (
     <section className="login-container">
       <div className="login-card">
         <h1 className="login-card-header">Login</h1>
-        <form className="login-card-form">
+        <form onSubmit={handleLogin} className="login-card-form">
           <div className="field-wrapper">
-            <label htmlFor="email-field">Enter your email address</label>
+            <label htmlFor="email-field">Email</label>
             <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               name="email"
               id="email-field"
-              placeholder="Email address"
+              placeholder="janedoe@example.com"
             />
           </div>
           <div className="field-wrapper">
-            <label htmlFor="password-field">Enter your password</label>
+            <label htmlFor="password-field">Password</label>
             <input
-              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type={isPasswordShown ? "text" : "password"}
               name="password"
               id="password-field"
-              placeholder="Password"
+              placeholder="Enter your password"
             />
+            <div className="show-password-wrapper">
+              <button
+                type="button"
+                onClick={() => setIsPasswordShown(!isPasswordShown)}
+                id="show-password-button"
+              >
+                {isPasswordShown ? (
+                  <EyeSlashIcon color="#868686" weight="fill" size={24} />
+                ) : (
+                  <EyeIcon color="#868686" weight="fill" size={24} />
+                )}
+              </button>
+            </div>
           </div>
           <button type="submit" id="login-button">
             Log in
@@ -28,7 +83,7 @@ export default function Login() {
         </form>
         <div className="login-card-footer">
           <a href="">Register</a>
-          <a href="">Forgotten password?</a>
+          <a href="">Forgot your password?</a>
         </div>
       </div>
     </section>
